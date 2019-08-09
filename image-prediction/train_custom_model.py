69db2196e8255b335a7aa58ab11e4f3d09e60454
ProfessorChen <mulThree@163.com>
2019-08-10 01:15:39 +0800

    update

--- image-prediction/train_custom_model.py
+++ image-prediction/train_custom_model.py
@@ -6,5 +6,5 @@
 from imageai.Prediction.Custom import ModelTraining
 model_trainer = ModelTraining()
 model_trainer.setModelTypeAsResNet()
-model_trainer.setDataDirectory("./pic/flower")
+model_trainer.setDataDirectory()
 model_trainer.trainModel(num_objects=5, num_experiments=100, enhance_data=True, batch_size=48, show_network_summary=True)
