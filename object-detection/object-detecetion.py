18ca75e8924b6680b7fce49b878e7f9ea6f8a7b1
ProfessorChen <mulThree@163.com>
2019-08-13 22:11:32 +0800

    update object-detection.fileArray()

--- object-detection/object-detecetion.py
+++ object-detection/object-detecetion.py
@@ -34,12 +34,10 @@ def fileArray(detector):
     input_image_list = sorted(input_image_list, key=lambda i: len(i), reverse=False)
     size = len(input_image_list)
     for i in range(size):
-        input_image_path = os.path.join(path + "\pic\input", input_image_list[i])
-        output_image_path = os.path.join(path + "\pic\output", input_image_list[i])
-        imgArray = plt.imread(input_image_path)
+        input_image_path = "./pic/input/" + input_image_list[i]
+        imgArray = cv2.imread(input_image_path)
         det, obj = detector.detectObjectsFromImage(input_image=imgArray,
-                                                   input_type='array',
-                                                   output_type='array')
+                                                   input_type="array", output_type="array")
         detections.append(det)
         extract_detected_objects.append(obj)
     return detections, extract_detected_objects
@@ -49,17 +47,18 @@ def main():
     detector.setModelTypeAsYOLOv3()
     detector.setModelPath("./model/yolo.h5")
     detector.loadModel("fast")
-    detections, extract_detected_objects = fileStream(detector)
+    detections, extract_detected_objects = fileArray(detector)
     # print(detections)
     size = len(detections)
-    for i in range(size):
-        print('------------------- %d -------------------' % int(i + 1))
-        for eachObject in detections[i]:
-            print(eachObject["name"], " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"])
-        print('------------------- %d -------------------' % int(i + 1))
     # for i in range(size):
-    #     plt.imshow(detections[i])
-    #     plt.show()
+    #     print('------------------- %d -------------------' % int(i + 1))
+    #     for eachObject in detections[i]:
+    #         print(eachObject["name"], " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"])
+    #     print('------------------- %d -------------------' % int(i + 1))
+    for i in range(size):
+        cv2.imshow('pic',detections[i])
+        cv2.waitKey()
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
